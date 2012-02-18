0fbdda411bb5bc688c26252effa4e927a5f03fb4
pwx <airyai@gmail.com>
2012-02-18 14:26:15 +0800

    build repository

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: pwx
+ *
+ * Created on 2012年2月18日, 下午2:21
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
