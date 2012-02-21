90eccfc492530516ecf068297c42c4731cb89990
pwx <airyai@gmail.com>
2012-02-21 22:47:19 +0800

    Switch to eclipse; rename Controller to SheetCtl; finished SheetCtl untested.

---
+++ pwxget.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : pwxget.cpp
+// Author      : PWX
+// Version     :
+// Copyright   : Copy PWX's Workshop 2012
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include "test.h"
+using namespace std;
+
+int main() {
+	test_all();
+	return 0;
+}
